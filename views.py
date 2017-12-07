da733ccc14a6ef31c5ef1b60d878d7502b3b5153
eugenecho2911 <34239667+eugenecho2911@users.noreply.github.com>
2017-12-07 11:26:57 +0400

    Update views.py

--- views.py
+++ views.py
@@ -17,9 +17,17 @@ class ObserveWaitPage(WaitPage):
 
 class Observe(Page):
     form_model = models.Player
-    form_fields = ['Score_p1', 'Score_p2', 'Score_p3']
+    form_fields = ['eval_p1', 'eval_p2', 'eval_p3']
+
+    def vars_for_template(self):
+        return {
+            'p1_contrib':self.group.get_player_by_id(1).contribution,
+            'p2_contrib':self.group.get_player_by_id(2).contribution,
+            'p3_contrib':self.group.get_player_by_id(3).contribution,
+        }
 
 class ResultsWaitPage(WaitPage):
+
     def after_all_players_arrive(self):
         self.group.set_payoffs()
     body_text = "Waiting for other participants to evaluate."
@@ -29,18 +37,19 @@ class Results(Page):
     """Players payoff: How much each has earned"""
     def vars_for_template(self):
         return {
-            'Your Contribution':
-            'Other Player 1 Contribution':
-            'Other Player 2 Contribution':
-            'Total Contribution':
-            'Your Score':
-            'Other Player 1 Score':
-            'Other Player 2 Score':
-            'Median Score':
-            'Your Remaining Tokens':
-            'Group Grade':
-            'Your Individual Grade'
-            'Your Final Grade':
+            'Your Contribution': p1_contrib,
+            'Other Player 1 Contribution':None,
+            'Other Player 2 Contribution':None,
+            'Total Contribution':None,
+            'Your Score':None,
+            'Other Player 1 Score':None,
+            'Other Player 2 Score':None,
+            'Median Score':None,
+            'Your Remaining Tokens':None,
+            'Group Grade':None,
+            'Your Individual Grade':None,
+            'Your Final Grade':None,
+            'diff_endow_contrib':Constants.endowment - self.player.units
         }
 
 page_sequence = [
@@ -51,3 +60,4 @@ page_sequence = [
     ResultsWaitPage,
     Results
 ]
+
